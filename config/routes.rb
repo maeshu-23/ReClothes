d2064a086bc0c7c68a3c8b4976b996cfd2175603
maeshu-23 <maeshu619@gmail.com>
2020-08-17 16:10:46 +0900

    add flash-message

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  get 'messages/index'
-  root "messages#index"
+  get 'home/index'
+  devise_for :users
+  # get 'messages/index'
+  # root "messages#index"
+  root to: "home#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
